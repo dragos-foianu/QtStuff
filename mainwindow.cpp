2102f0fb7cfbfacb78b6ab5e26d3d2f292acb1f4
Dragos Foianu <dragos.foianu@gmail.com>
2016-07-01 13:47:32 +0300

    Basic interface, non-functional

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,9 +6,55 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    this->setCentralWidget(ui->textEdit);
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_actionNew_triggered()
+{
+
+}
+
+void MainWindow::on_actionOpen_triggered()
+{
+
+}
+
+void MainWindow::on_actionSave_triggered()
+{
+
+}
+
+void MainWindow::on_actionSave_As_triggered()
+{
+
+}
+
+void MainWindow::on_actionUndo_triggered()
+{
+
+}
+
+void MainWindow::on_actionRedo_triggered()
+{
+
+}
+
+void MainWindow::on_actionCut_triggered()
+{
+
+}
+
+void MainWindow::on_actionCopy_triggered()
+{
+
+}
+
+void MainWindow::on_actionPaste_triggered()
+{
+
 }
